a3325ee97dd89a6b6c0c8f915461e50f6b0622c1
Giorgi.Gagnidze <giorgi.gagnidze@voidaint.com>
2025-04-22 12:48:21 +0400

    add health check and unit test

---
+++ src/test/java/ge/kiu/devops/web/TodoControllerTest.java
@@ -0,0 +1,24 @@
+package ge.kiu.devops.web;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+@WebMvcTest(TodoController.class)
+public class TodoControllerTest {
+
+  @Autowired
+  private MockMvc mockMvc;
+
+  @Test
+  public void testGetTasksRoute() throws Exception {
+    mockMvc.perform(get("/tasks"))
+        .andExpect(status().isOk())
+        .andExpect(view().name("tasks"));
+  }
+}
